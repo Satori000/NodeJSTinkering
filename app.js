f1368a31bc163ff52ce4c98502cca6e38074a216
Satori <shakeebmajid@ufl.edu>
2016-08-13 01:40:35 -0400

    created a server and served data from mongodb

--- app.js
+++ app.js
@@ -1,19 +1,11 @@
+var http = require("http");
+
 var MongoClient = require('mongodb').MongoClient
   , assert = require('assert');
 
 // Connection URL
 var url = 'mongodb://localhost:27017/nodejs';
 
-// Use connect method to connect to the server
-MongoClient.connect(url, function(err, db) {
-  assert.equal(null, err);
-  console.log("Connected succesfully to server");
-
-
-
-
-  db.close();
-});
 
 var insertDocuments = function(db, callback) {
   // Get the documents collection
@@ -30,18 +22,6 @@ var insertDocuments = function(db, callback) {
   });
 }
 
-
-var url = 'mongodb://localhost:27017/nodejs';
-// Use connect method to connect to the server
-MongoClient.connect(url, function(err, db) {
-  assert.equal(null, err);
-  console.log("Connected successfully to server");
-
-  insertDocuments(db, function() {
-    db.close();
-  });
-});
-
 var findDocuments = function(db, callback) {
   // Get the documents collection
   var collection = db.collection('documents');
@@ -49,18 +29,81 @@ var findDocuments = function(db, callback) {
   collection.find({}).toArray(function(err, docs) {
     assert.equal(err, null);
     console.log("Found the following records");
-    console.log(docs)
+    //console.log(docs)
     callback(docs);
+
   });
 }
 
-MongoClient.connect(url, function(err, db) {
+
+var removeDocument = function(db, callback) {
+  // Get the documents collection
+  var collection = db.collection('documents');
+  // Insert some documents
+  collection.deleteOne({ a : 1 }, function(err, result) {
+    assert.equal(err, null);
+    assert.equal(1, result.result.n);
+    console.log("Removed the document with the field a equal to 1");
+    callback(result);
+  });
+}
+
+var indexCollection = function(db, callback) {
+  db.collection('documents').createIndex(
+    { "h": 2 },
+      null,
+      function(err, results) {
+        console.log(results);
+        callback();
+    }
+  );
+};
+
+
+http.createServer(function (request, response) {
+
+   // Send the HTTP header
+   // HTTP Status: 200 : OK
+   // Content Type: text/plain
+   MongoClient.connect(url, function(err, db) {
   assert.equal(null, err);
   console.log("Connected correctly to server");
+  /*removeDocument(db, function() {
 
-  insertDocuments(db, function() {
-    findDocuments(db, function() {
-      db.close();
-    });
-  });
+  }); */
+
+
+  	insertDocuments(db, function() {
+
+  		indexCollection(db, function() {
+
+    		findDocuments(db, function(docs) {
+    			console.log("hello")
+          console.log(docs)
+          //var string = JSON.parse(docs)
+          response.writeHead(200, {'Content-Type': 'text/plain'});
+
+
+          docs.forEach(function(doc) {
+            response.write(" " + doc.a )
+          });
+
+          response.end()
+
+          // Send the response body as "Hello World"
+          //response.end( " " + docs[1].a);
+
+      			db.close();
+    		});
+  		});
+  	});
 });
+
+
+
+   //response.writeHead(404, {'Content-Type': 'text/html'});
+
+   // Send the response body as "Hello World"
+   //response.end('<html><body><b>Hello World</b></body></html>');
+
+}).listen(8081);
